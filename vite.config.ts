551554a5fd06e518a621e741a7a8832ca7900682
Spotted <spotted.contato.oficial@gmail.com>
2025-09-09 07:56:52 -0300

    <UPDATE - Config>: Deploy.yml

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/Portfolio/",
   server: {
     open: true, // Navegador abra automaticamente
     port: 5173 // Ou qualquer outra porta que quiser
